575353cb850e5e1627e81bb57bfe266634d4473d
WesleyCrist <wesleycristiano@outlook.com>
2022-12-02 10:13:56 -0300

    Rota percent finalizada

--- src/routes/porcentagem/percent.routes.js
+++ src/routes/porcentagem/percent.routes.js
@@ -3,40 +3,38 @@ import Calculator from '../../functions/Calculator.js'
 
 const percent = express()
 
-// Rota para o caso de nenhum envio de números para calculo
-percent.get('/', (req, res) => {
-    res.send({
-        message: 'os parâmentros precisam ser preenchidos',
-        status: 'ainda a modificar'
-    })
-})
+percent.get('/:x?/:y?', (req, res) => {
 
-// Rota para calculo de porcentagem
-percent.get('/:x/:y', (req, res) => {
-
-    const valX = Number(req.params.x)
-    const valY = Number(req.params.y)
-    const result = Calculator.percent(valX, valY)
-    if(!isNaN(valX) && !isNaN(valY)) {
-        try {
-            res.status(200).send({
-                message: `${valX}% de ${valY} é ${result}`,
-                X: req.params.x,
-                Y: req.params.y,
-                Resultado: result,
-                status: 200
-            })
-        } catch(e) {
-            res.status(500).send({
-                message: 'Falha na execução...',
-                error: 'e.message'
-            })
-        }
-    } else {
+    if(!req.params.x && !req.params.y) {
         res.status(400).send({
-            message: 'Os atributos enviados pela url não podem ser Strings, apenas number',
-            codeStatus: 400
+            message: 'Ambos os parâmetro precisam ser preenchidos',
+            status: 'ainda a modificar'
         })
+    } else{
+        const valX = Number(req.params.x)
+        const valY = Number(req.params.y)
+        if(isNaN(valX) || isNaN(valY)) {
+            res.status(400).send({
+                message: 'Nenhum parâmetro deve estar fazio ou receber strings, ambos recebem number',
+                codeStatus: 400
+            })
+        } else {
+            try {
+                const result = Calculator.percent(valX, valY)
+                res.status(200).send({
+                    message: `${valX}% de ${valY} é ${result}`,
+                    X: valX,
+                    Y: valY,
+                    Resultado: result,
+                    status: 200
+                })
+            } catch(e) {
+                res.status(500).send({
+                    message: 'Falha na execução',
+                    error: e.message
+                })
+            }
+        }
     }
 })
 
